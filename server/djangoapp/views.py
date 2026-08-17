07232cd8817651b08c84c84133462b91fb6fa436
Juanda-77 <119276282+Juanda-77@users.noreply.github.com>
2026-08-16 22:45:02 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,13 +1,12 @@
 # Uncomment the required imports before adding the code
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
+# from django.shortcuts import get_object_or_404, render, redirect
+# from django.contrib.auth import logout
+# from django.contrib import messages
+# from datetime import datetime
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -17,15 +16,9 @@ from .populate import initiate
 from .models import CarMake, CarModel
 from .restapis import get_request, analyze_review_sentiments, post_review
 
-
-
-
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
-
 # Create your views here.
-
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
@@ -43,16 +36,15 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-def logout_request(request): # Terminate user session
-    data = {"userName":""} # Return empty username
+def logout_request(request):  # Terminate user session
+    data = {"userName": ""}  # Return empty username
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
     context = {}
-
-	# Load JSON data from the request body
+# Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -60,25 +52,28 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+	except Exception:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(username=username, 
+										first_name=first_name, 
+										last_name=last_name, 
+										password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status":"Authenticated"}
         return JsonResponse(data)
     else :
-        data = {"userName":username,"error":"Already Registered"}
+        data = {"userName": username, "error":"Already Registered"}
         return JsonResponse(data)
 
 def get_cars(request):
