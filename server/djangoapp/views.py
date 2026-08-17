457a056abbb0e70487c13baebfb07de7c6ca88e3
Juanda-77 <119276282+Juanda-77@users.noreply.github.com>
2026-08-17 13:00:27 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -57,7 +57,7 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-	except Exception:  # If not, simply log this is a new user
+    except Exception:  # If not, simply log this is a new user
 		logger.debug("{} is new user".format(username))
 
     # If it is a new user
